847a2047d0e4c3d3f0b429b70e428695a7324327
Sean McGroarty <mcgroas@tcd.ie>
2017-11-09 14:34:00 +0000

    added: arguments to except directory to execute argon on

--- app/Main.hs
+++ app/Main.hs
@@ -8,26 +8,44 @@ import Control.Monad
 import Network.Transport.TCP                              (createTransport,defaultTCPParameters)
 import System.Environment                                 (getArgs)
 import System.Exit
+import System.FilePath
 
 import Prelude hiding (log)
 
 import Lib 
 import Utils
 
+port :: String
+port = "600"
+
+startWorkers :: Int -> String -> IO ()
+startWorkers 0 _ = return ()
+startWorkers n host = do
+  putStrLn $ port ++ show n
+  backend <- initializeBackend host (port ++ show n) rtable
+  startSlave backend
+  startWorkers (n -1) host
+    
+startManager :: Files -> String -> IO ()
+startManager files host = do
+  backend <- initializeBackend host "5000" rtable
+  startMaster backend $ \workers -> do
+    result <- manager files workers
+    plog result
+    plog " Terminating slaves..."
+    terminateAllSlaves backend
+
 main :: IO ()
 main = do
   args <- getArgs
   let files = getFiles
-  case args of 
-    ["manager", host, port] -> do
-      log "Starting manager node..."
-      backend <- initializeBackend host port rtable
-      startMaster backend $ \workers -> do
-        result <- manager files workers
-        plog result
-        plog " Terminating slaves..."
-        terminateAllSlaves backend
+  case args of
     ["worker", host, port] -> do
+      putStrLn "Starting Node as Worker"
       backend <- initializeBackend host port rtable
-      startSlave backend
+      startSlave backend    
+    ["argon", path, host, n] -> do 
+      files <- splitFiles path
+      --startWorkers (read n :: Int) host
+      startManager files host
     _ -> putStrLn "Bad parameters"

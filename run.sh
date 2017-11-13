edd7bed4f5b0754f3c01cc248e53dcf64cec0bf3
Sean McGroarty <mcgroas@tcd.ie>
2017-11-13 20:36:43 +0000

    updated: arguments passed in

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 stack exec Distributed-Argon worker localhost 7000 &
 stack exec distributed-Argon worker localhost 7001 &
 stack exec Distributed-Argon worker localhost 7002 &
-stack exec Distributed-Argon argon $1 localhost 
+stack exec Distributed-Argon manager $1 localhost 7003 

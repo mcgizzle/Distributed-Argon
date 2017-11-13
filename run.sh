f2fb71ef95ea243085381d65a5b0a6321ac30470
Sean McGroarty <mcgroas@tcd.ie>
2017-11-13 12:02:06 +0000

    added: directory as variable passed in

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 stack exec Distributed-Argon worker localhost 7000 &
 stack exec distributed-Argon worker localhost 7001 &
 stack exec Distributed-Argon worker localhost 7002 &
-stack exec Distributed-Argon argon ../../FYP/Stack/src/Stack localhost $1 
+stack exec Distributed-Argon argon $1 localhost 

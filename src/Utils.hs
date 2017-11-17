d4bfc1ec9c1d40a434deac58713a8657e813bc03
Sean McGroarty <mcgroas@tcd.ie>
2017-11-17 19:53:48 +0000

    updated: commands

--- src/Utils.hs
+++ src/Utils.hs
@@ -7,7 +7,8 @@ runArgon
 import System.IO
 import Control.Monad.State
 import System.FilePath
-import Control.Distributed.Process
+import Control.Distributed.Process hiding (try)
+import Control.Exception
 import Prelude hiding (log)
 import System.Process
 import System.Directory
@@ -25,8 +26,12 @@ type Command = (String,String)
 runArgon :: String -> IO String
 runArgon file = sendCommand ("stack","exec argon "++file)
 
+try' :: IO a ->  IO (Either IOException a)
+try' =  try 
+
 sendCommand :: Command -> IO String
 sendCommand (cmd,arg) = do
-  (_,Just hout,_,_)  <- createProcess (proc cmd $ words arg){ std_out = CreatePipe }
+  (_,Just hout,_,ph) <- createProcess (proc cmd $ words arg){ std_out = CreatePipe }
   hGetContents hout
+
 

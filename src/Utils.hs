c0a0df74257a5d9b26fba7df664f0c68a583db6e
Sean McGroarty <mcgroas@tcd.ie>
2017-11-23 16:38:17 +0000

    added: git commands

--- src/Utils.hs
+++ src/Utils.hs
@@ -13,6 +13,8 @@ import Prelude hiding (log)
 import System.Process
 import System.Directory
 
+import System.IO.Temp
+
 -- LOG
 log :: String -> IO ()
 log = liftIO . putStrLn 
@@ -23,15 +25,32 @@ plog msg = say $ "-->" ++ msg
 -- COMMANDS
 type Command = (String,String)
 
-runArgon :: String -> IO String
-runArgon file = sendCommand ("stack","exec argon "++file)
-
-try' :: IO a ->  IO (Either IOException a)
-try' =  try 
-
 sendCommand :: Command -> IO String
 sendCommand (cmd,arg) = do
   (_,Just hout,_,ph) <- createProcess (proc cmd $ words arg){ std_out = CreatePipe }
   hGetContents hout
 
+sendCommand_ :: Command -> IO ()
+sendCommand_ (cmd,arg) = createProcess (proc cmd $ words arg) >> return ()
+ 
+-- UTIL COMMANDS
+cd :: String -> IO ()
+cd dir = sendCommand_ ("cd",dir)
+
+-- ARGON
+runArgon :: String -> IO String
+runArgon file = sendCommand ("stack","exec argon "++file)
+
+-- GIT
+cloneRepo :: String -> IO ()
+cloneRepo repo = sendCommand_ ("git","clone "++repo)
+
+getCommits :: String -> IO [String]
+getCommits repo = do
+  cd repo
+  commits <- sendCommand ("git","log --pretty=format:'%H'")
+  return $ lines commits
+
+cloneCommit :: String -> IO ()
+cloneCommit commit = cd commit >> sendCommand_ ("git","reset --hard "++commit)
 

5c8134446e3f4f5a98b946902c1b47cb1a238257
Sean McGroarty <mcgroas@tcd.ie>
2017-11-24 11:37:23 +0000

    fixed: git reset bug

--- src/Utils.hs
+++ src/Utils.hs
@@ -16,11 +16,6 @@ import System.Directory
 
 import System.IO.Temp
 
-strip :: [a] -> [a]
-strip []  = []
-strip [x] = []
-strip xs  = tail (init xs)
-
 -- LOG
 log :: String -> IO ()
 log = liftIO . putStrLn 
@@ -36,10 +31,6 @@ sendCommand (cmd,arg) = do
   (_,Just hout,_,ph) <- createProcess (proc cmd $ words arg){ std_out = CreatePipe }
   hGetContents hout
 
--- UTIL COMMANDS
-cd :: String -> IO ()
-cd dir = callProcess "cd" [dir]
-
 -- ARGON
 runArgon :: String -> IO String
 runArgon file = sendCommand ("stack","exec argon "++file)
@@ -51,18 +42,23 @@ cloneRepo :: String -> String -> IO ()
 cloneRepo url dir = do
   exists <- doesDirectoryExist dir
   unless exists $ callProcess "git" ["clone",url]
-  cd dir
-  putStrLn $ "Directory: " ++ dir 
   return ()
 
 getCommits :: String -> String -> IO [String]
 getCommits url dir = do
   cloneRepo url dir
-  commits <- sendCommand ("git","--git-dir "++ dir ++".git log --pretty=format:'%H'")
+  commits <- sendCommand ("git","--git-dir "++ dir ++"/.git log --pretty=format:'%H'")
   return $ map strip $ words commits
 
 fetchCommit :: Repo -> IO ()
 fetchCommit (url,dir,commit) = do
   cloneRepo url dir
-  readCreateProcess ((proc "git" ["reset","--hard","--git-dir",dir ++".git",commit]){ cwd = Just dir}) ""
+  readCreateProcess ((proc "git" ["reset","--hard",commit]){ cwd = Just dir}) ""
   return ()
+
+strip :: [a] -> [a]
+strip []  = []
+strip [x] = []
+strip xs  = tail (init xs)
+
+

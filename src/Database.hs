2e73f9e03bbc5b453c3c9d9ccf72b6439aa6f05a
Sean McGroarty <mcgroas@tcd.ie>
2017-11-26 15:14:25 +0000

    added: relation for compareing speed of number of nodes

--- src/Database.hs
+++ src/Database.hs
@@ -15,11 +15,13 @@
 {-# LANGUAGE TypeFamilies               #-}
 
 module Database(
+Run(..),
 initDB,runDB,
-insertStartTime,insertEndTime,
+insertStartTime,insertEndTime,insertTotalStartTime,insertTotalEndTime,
 insertFile,insertResult,
 fetchTotal
 ) where
+
 
 -- PERSISTENT
 import Control.Monad.IO.Class
@@ -41,21 +43,38 @@ import Data.ByteString (ByteString)
 import Data.Time.LocalTime
 import Data.Time
 
-
+-- MISC
+import Data.Maybe (fromJust)
 -- TODO -> Create data type for storing the complexity 
 
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Repository json
+    url String
+    nodes Int
+    start_time UTCTime
+    end_time UTCTime Maybe
+    deriving Show
 CommitInfo json
+    repositoryId RepositoryId
     commit_str String
     start_time UTCTime 
     end_time UTCTime Maybe
     deriving Show
 CommitResults json
+    repositoryId RepositoryId
     commit_str String
     file_name String
     complexity String Maybe
     deriving Show
 |]
+
+data Run = Run {
+  url :: String,
+  dir :: FilePath,
+  nodes :: Int,
+  commit :: String,
+  id :: Key Repository
+}
 
 connStr = "host=localhost dbname=argon_test user=root password=root port=5432" 
 
@@ -72,22 +91,41 @@ runDB pool query = liftIO $ runSqlPool query pool
 
 doMigrations = runMigration migrateAll
 
-insertStartTime commit = do
+insertStartTime id commit = do
   time <- liftIO getCurrentTime
-  insert $ CommitInfo commit time Nothing  
+  insert $ CommitInfo id commit time Nothing  
   return ()
 
-insertEndTime commit = do
+insertEndTime id commit = do
   time <- liftIO getCurrentTime
-  updateWhere [CommitInfoCommit_str ==. commit ] [CommitInfoEnd_time =. Just time]
+  updateWhere [ CommitInfoRepositoryId ==. id
+              , CommitInfoCommit_str ==. commit ] 
+              [ CommitInfoEnd_time =. Just time ]
   return ()
 
-insertFile commit file = do
-  insert $ CommitResults commit file Nothing 
+insertFile id commit file = do
+  insert $ CommitResults id commit file Nothing 
   return ()
 
-insertResult commit (file,res) = do
-  updateWhere [CommitResultsFile_name ==. file] [CommitResultsComplexity =. Just res]
+insertResult id commit file res = do
+  updateWhere [ CommitResultsRepositoryId ==. id
+              , CommitResultsFile_name ==. file ] 
+              [CommitResultsComplexity =. Just res]
   return ()
 
-fetchTotal commit = count [CommitResultsCommit_str ==. commit] 
+insertTotalStartTime url nodes = do
+  time <- liftIO getCurrentTime
+  id <- insert $ Repository url nodes time Nothing
+  return id
+
+insertTotalEndTime url nodes = do
+  time <- liftIO getCurrentTime
+  updateWhere [ RepositoryNodes ==. nodes
+              , RepositoryUrl ==. url] 
+              [RepositoryEnd_time =. Just time]
+  return ()
+
+
+fetchTotal id commit = count [ CommitResultsRepositoryId ==. id
+                             , CommitResultsCommit_str ==. commit]
+

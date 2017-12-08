c9819168ad2fae1414afa2efe1a316b848c1cec5
Sean McGroarty <mcgroas@tcd.ie>
2017-12-08 11:54:57 +0000

    fixed: compiling

--- src/Utils.hs
+++ src/Utils.hs
@@ -1,3 +1,15 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE TypeFamilies               #-}
+
 module Utils(
 plog,log,
 Command,
@@ -8,13 +20,56 @@ Repo,getCommits,fetchCommit
 import System.IO
 import Control.Monad.State
 import System.FilePath
-import Control.Distributed.Process hiding (try)
 import Control.Exception
 import Prelude hiding (log)
 import System.Process
 import System.Directory
 
 import System.IO.Temp
+import Control.Monad.Reader
+import Control.Distributed.Process
+
+import Data.List.Split
+
+import Database.Persist.Sql (runSqlPool)
+import Control.Monad.Reader (MonadIO, MonadReader, asks, liftIO)
+import Database.Persist.Sql (SqlPersistT, runMigration, runSqlPool)
+import Database.Persist.TH  (mkMigrate, mkPersist, persistLowerCase,
+                                       share, sqlSettings)
+import Database.Persist.Postgresql (ConnectionPool, ConnectionString,
+                                    createPostgresqlPool)
+import Control.Monad.Logger (runNoLoggingT, runStdoutLoggingT)
+import Control.Monad.Reader (MonadReader, ReaderT, asks, runReaderT,MonadIO)
+
+-- Reader
+data AppConfig = AppConfig {
+                              poolConfig :: ConnectionPool,
+                              urlConfig :: String,
+                              commitConfig :: String,
+                              idConfig :: Int              
+                           }
+type AppProcess = ReaderT AppConfig Process
+
+--connStr :: ConnectionString
+connStr = "host=localhost dbname=fs_dev user=root password=root port=5432"
+
+initConfig :: String -> String -> Int -> IO AppConfig
+initConfig commit url id = do
+        p <- makePool 
+        return AppConfig {
+                poolConfig = p,
+                urlConfig = url,
+                commitConfig = commit,
+                idConfig = id
+                          }
+makePool :: IO ConnectionPool
+makePool = do
+        conn <- getConnString
+        runStdoutLoggingT (createPostgresqlPool conn 1)
+
+
+getConnString :: IO ConnectionString
+getConnString = return connStr
 
 -- LOG
 log :: String -> IO ()
@@ -31,29 +86,32 @@ sendCommand (cmd,arg) = do
   (_,Just hout,_,ph) <- createProcess (proc cmd $ words arg){ std_out = CreatePipe }
   hGetContents hout
 
+getDir :: String -> String
+getDir = last . splitOn "/" 
+
 -- ARGON
 runArgon :: String -> IO String
 runArgon file = sendCommand ("stack","exec -- argon --json "++file)
 
 -- GIT
-type Repo = (String,String,String)
+type Repo = (String,String)
 
-cloneRepo :: String -> String -> IO ()
-cloneRepo url dir = do
-  exists <- doesDirectoryExist dir
+cloneRepo :: String -> IO ()
+cloneRepo url = do
+  exists <- doesDirectoryExist $ getDir url
   unless exists $ callProcess "git" ["clone",url]
   return ()
 
-getCommits :: String -> String -> IO [String]
-getCommits url dir = do
-  cloneRepo url dir
-  commits <- sendCommand ("git","--git-dir "++ dir ++"/.git log --pretty=format:'%H'")
+getCommits :: String -> IO [String]
+getCommits url = do
+  cloneRepo url
+  commits <- sendCommand ("git","--git-dir "++ (getDir url) ++"/.git log --pretty=format:'%H'")
   return $ map strip $ words commits
 
-fetchCommit :: Repo -> IO ()
-fetchCommit (url,dir,commit) = do
-  cloneRepo url dir
-  readCreateProcess ((proc "git" ["reset","--hard",commit]){ cwd = Just dir}) ""
+fetchCommit :: (String,String) -> IO ()
+fetchCommit (url,commit) = do
+  cloneRepo url
+  readCreateProcess ((proc "git" ["reset","--hard",commit]){ cwd = Just (getDir url)}) ""
   return ()
 
 strip :: [a] -> [a]
